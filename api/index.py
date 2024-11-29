5e6f9cae29535ea93e50950566f7a14121de4886
WoongyuChoi <schna@kakao.com>
2024-11-29 18:34:02 +0900

    fix : favicon.ico 처리

--- api/index.py
+++ api/index.py
@@ -8,9 +8,20 @@ def home():
     return jsonify(message="Hello, Vercel!")
 
 
+@app.route("/favicon.ico")
+def favicon():
+    return "", 204
+
+
 def handler(event, context):
+    from werkzeug.middleware.dispatcher import DispatcherMiddleware
     from werkzeug.wrappers import Request, Response
 
+    dispatcher = DispatcherMiddleware(app)
     request = Request(event)
-    response = Response.from_app(app, request.environ)
-    return response(environ=None, start_response=None)
+    response = Response.from_app(dispatcher, request.environ)
+    return response(environ=None, start_response=lambda *args: None)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)

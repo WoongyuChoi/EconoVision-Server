93ce5ebbf189233270a977117318323674585173
WoongyuChoi <schna@kakao.com>
2024-12-02 15:03:54 +0900

    fix : Logger 레벨 설정

--- handler/logger.py
+++ handler/logger.py
@@ -1,17 +1,24 @@
 import logging
+import os
 
 
-def get_logger(name: str, level: str = "INFO") -> logging.Logger:
+def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
-    logger.setLevel(level)
 
-    if not logger.hasHandlers():
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(level)
-        formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-        )
-        console_handler.setFormatter(formatter)
-        logger.addHandler(console_handler)
+    if logger.hasHandlers():
+        logger.handlers.clear()
+
+    log_level = os.getenv(
+        "LOG_LEVEL", "INFO" if os.getenv("VERCEL_ENV") != "production" else "WARNING"
+    )
+    logger.setLevel(log_level)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(log_level)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
 
     return logger

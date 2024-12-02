66af06d091ec7215617cc115719d4a374f8444f9
WoongyuChoi <schna@kakao.com>
2024-12-02 13:45:32 +0900

    feature : 대원화환율 API 연동

--- api/server.py
+++ api/server.py
@@ -1,7 +1,9 @@
 import time
-from flask import Flask, jsonify
-from api.external import check_ecos
-from api.logger import get_logger
+
+from flask import Flask, jsonify, request
+
+from api.external import check_ecos, fetch_exchange_rate
+from handler.logger import get_logger
 
 app = Flask(__name__)
 logger = get_logger(__name__)
@@ -20,6 +22,28 @@ def health_check():
 @app.route("/favicon.ico")
 def favicon():
     return "", 204, {"Content-Type": "image/x-icon"}
+
+
+@app.route("/api/exchange-rate", methods=["GET"])
+def get_exchange_rate():
+    start_date = request.args.get("start_date")
+    end_date = request.args.get("end_date")
+    item_code = request.args.get("item_code", "0000001")
+
+    logger.debug(
+        f"Parameters received: start_date={start_date}, end_date={end_date}, item_code={item_code}"
+    )
+
+    try:
+        data = fetch_exchange_rate(start_date, end_date, item_code)
+        logger.info("Exchange rate data fetched successfully.")
+        return jsonify(data), 200
+    except ValueError as e:
+        logger.error(f"Error fetching exchange rate: {e}")
+        return jsonify({"error": str(e)}), 500
+    except Exception as e:
+        logger.error(f"Unexpected error: {e}")
+        return jsonify({"error": "An unexpected error occurred."}), 500
 
 
 def handler(event, context):

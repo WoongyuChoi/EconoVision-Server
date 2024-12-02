b9b044c83307fd13c3c273d283024be3a407567d
WoongyuChoi <schna@kakao.com>
2024-12-02 14:14:15 +0900

    fix : 중복 Logs 처리

--- handler/logger.py
+++ handler/logger.py
@@ -5,6 +5,9 @@ def get_logger(name: str, level: str = "INFO") -> logging.Logger:
     logger = logging.getLogger(name)
     logger.setLevel(level)
 
+    while logger.hasHandlers():
+        logger.removeHandler(logger.handlers[0])
+
     if not logger.hasHandlers():
         console_handler = logging.StreamHandler()
         console_handler.setLevel(level)
